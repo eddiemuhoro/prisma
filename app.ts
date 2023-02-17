0f6aa32df15879e1bdd4585862eea676100b5212
eddiemuhoro <eddiemuhoro@gmail.com>
2023-02-17 23:10:06 +0300

    added cors

--- app.ts
+++ app.ts
@@ -1,17 +1,37 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes/users';
-
+import cors from 'cors';
+import routerJob from './routes/job';
+import routerEmployee from './auth/employee';
 dotenv.config();
 
 const app = express();
 
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }
+))
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port =  3000;
+const port =  8000;
 
 app.use('/users', router);
+app.use('/job', routerJob);
+app.use('/employee', routerEmployee);
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
