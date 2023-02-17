3042cfd050bc16be5b38b5bfd86d5e0783144539
eddiemuhoro <eddiemuhoro@gmail.com>
2023-02-17 23:10:34 +0300

    simplified script file

--- script.ts
+++ script.ts
@@ -1,37 +1,7 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
 
-async function main() {
-  await prisma.user.deleteMany()
-  const users = await prisma.user.createMany({
-    data:[{
-      name: 'Alice',
-      email: 'alice@test.com',
-    },
-    {
-      name: 'Bob',
-      email: 'bob@test.com',
-    },
-    {
-      name: 'Charlie',
-      email: 'charlie@test.com',
-    },
-    {
-      name: 'Dave',
-      email: 'dave@test.com',
-    },
-  ]
-  })
-  console.log(users)
+const prisma = new PrismaClient();
 
-}
 
-main()
-    .catch(e => {
-        throw e
-    }
-    ) 
-    .finally(async () => {
-        await prisma.$disconnect()
-    }
-    )
+
+export default prisma
