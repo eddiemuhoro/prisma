8abe2dea7ba0ba9819d77255e42019d519782c3e
eddiemuhoro <eddiemuhoro@gmail.com>
2023-02-21 22:52:07 +0300

    get a single job based don it's id (findUnique)

--- routes/job.ts
+++ routes/job.ts
@@ -5,13 +5,33 @@ import prisma from '../script';
 const router = express.Router();
 
 router.post('/', async (req, res)=>{
-    const { title, description, employerId } = req.body;
+    const { title, description, employerId, skills } = req.body;
     const job = await prisma.job.create({
         data: {
-          title,
-          description,
-          employerId
+          title: "title",
+          description: "description",
+          employerId: 'fb28124d-b779-4f23-b587-c3318f335a30',
+          skills: ['HTML', 'CSS', 'JS']
         },
+    })
+    res.json(job);
+}
+)
+
+//get all jobs
+router.get('/', async (req, res)=>{
+    const jobs = await prisma.job.findMany();
+    res.json(jobs);
+}
+)
+
+//het a job by id
+router.get('/:id', async (req, res)=>{
+    const job = await prisma.job.findUnique({
+        where: {
+            //id is a string
+            id: String(req.params.id)
+        }
     })
     res.json(job);
 }
