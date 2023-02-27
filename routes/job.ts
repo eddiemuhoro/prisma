bfed326bf2d990c95a8c09b1fccf3585651cb329
eddiemuhoro <eddiemuhoro@gmail.com>
2023-02-27 22:33:24 +0300

    cleanup

--- routes/job.ts
+++ routes/job.ts
@@ -28,7 +28,7 @@ router.get('/', async (req, res)=>{
 //het a job by id
 router.get('/employer/:id', async (req, res)=>{
     const job = await prisma.job.findMany({
-        where: {
+        where:{
             //id is a string
             employerId: String(req.params.id)
         }
@@ -36,6 +36,8 @@ router.get('/employer/:id', async (req, res)=>{
     res.json(job);
 }
 )
+
+
 
 //get a single job from employer using job id
 router.get('/:id', async (req, res)=>{
@@ -50,15 +52,40 @@ router.get('/:id', async (req, res)=>{
 
 
 //update favorite job
-router.put('/favorite/:id', async (req, res)=>{
+router.put('/favorite/:id', async (req, res) => {
+    const { isFavorite, byFreelancer } = req.body;
     const job = await prisma.job.update({
         where: {
             id: String(req.params.id)
         },
-        //update favorite to true from frontend
         data: {
-            isFavorite: true
+            isFavorite: isFavorite,
+            favoritedBy: byFreelancer
+
         }
+    })
+    res.json(job);
+}
+)
+//fetch job where isFavorite is true
+router.get('/favorite/:id', async (req, res)=>{
+    const job = await prisma.job.findMany({
+        where: {
+            isFavorite: true,
+            favoritedBy: String(req.params.id)
+        }
+    })
+    res.json(job);
+}
+)
+
+//fetch the boolean value
+router.get('/favorite/:id', async (req, res)=>{
+    const job = await prisma.job.findUnique({
+        where: {
+            id: String(req.params.id)
+        },
+       
     })
     res.json(job);
 }
